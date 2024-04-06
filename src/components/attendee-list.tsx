99f9245e1cc2e4c85d85df3b30bb1bbe474c8582
Henrique Barbosa Sampaio <retr0lbb@gmail.com>
2024-04-06 01:49:44 -0300

    feat: add fake json data

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -1,21 +1,33 @@
+import { useState } from "react"
 import { Search, MoreHorizontal, ChevronsLeft, ChevronLeft, ChevronRight, ChevronsRight } from "lucide-react"
+import dayjs from "dayjs"
+import "dayjs/locale/pt-br"
+import relativeTime from "dayjs/plugin/relativeTime"
 import { IconButton } from "./icon-button"
 import { Table } from "./table/table"
 import { TableHeader } from "./table/table-header"
 import { TableCell } from "./table/table-cell"
 import { TableRow } from "./table/table-row"
+import { attendees } from "../data/attendees"
+
+
+dayjs.extend(relativeTime)
+dayjs.locale("pt-br")
+
 
 export function AttendeeList(){
+    const [search, setSearch] = useState("")
+    const [page, setPage] = useState(1)
+
     return(
         <div className="flex flex-col gap-4">
             <div className="flex items-center gap-3 ">
                 <h1 className="text-2xl font-bold">Participantes</h1>
                 <div className="flex items-center px-3 w-72 py-1.5 border border-white/10 rounded-lg gap-3">
                     <Search className="size-4 text-emerald-300" />
-                    <input type="text" className="bg-transparent flex-1 outline-none h-auto border-0 p-0 text-sm" placeholder="Buscar participante..." />
+                    <input onChange={(e) => {setSearch(e.target.value)}} value={search} className="bg-transparent flex-1 outline-none h-auto border-0 p-0 text-sm" placeholder="Buscar participante..." />
                 </div>
             </div>
-
             
             <Table>
                 <thead>
@@ -31,21 +43,21 @@ export function AttendeeList(){
                     </tr>
                 </thead>
                 <tbody>
-                    {Array.from({length: 8}).map((_, i) => {
+                    {attendees.slice((page - 1) *10, page *10).map((attendee) => {
                         return(
-                            <TableRow>
+                            <TableRow key={attendee.id}>
                                 <TableCell>
                                     <input type="checkbox" className="size-4 bg-black/20 rounded border border-white/10" />
                                 </TableCell>
-                                <TableCell>123</TableCell>
+                                <TableCell>{attendee.id}</TableCell>
                                 <TableCell>
                                 <div className="flex flex-col gap-1">
-                                    <span className="font-semibold text-white">Henrique Barbosa Sampaio</span>
-                                    <span>retr0lbb@gmail.com</span>
+                                    <span className="font-semibold text-white">{attendee.name}</span>
+                                    <span>{attendee.email}</span>
                                 </div>
                                 </TableCell>
-                                <TableCell>7 dia atrás</TableCell>
-                                <TableCell>4 dias atrás</TableCell>
+                                <TableCell>{dayjs().to(attendee.createdAt)}</TableCell>
+                                <TableCell>{dayjs().to(attendee.checkedInAt)}</TableCell>
                                 <TableCell>
                                     <IconButton transparent>
                                         <MoreHorizontal className="size-4" />
@@ -58,11 +70,11 @@ export function AttendeeList(){
                 <tfoot>
                     <tr>
                         <TableCell colSpan={3}>
-                            Mostrando 10 de 228 items
+                            Mostrando 10 de {attendees.length} items
                         </TableCell>
                         <TableCell className="text-right" colSpan={3}>
                             <div className="inline-flex gap-8 items-center ">
-                                <span>Página 1 de 23</span>
+                                <span>Página {page} de {Math.ceil(attendees.length / 10)}</span>
                                 <div className=" flex gap-1.5">
                                     <IconButton>
                                         <ChevronsLeft className="size-4" />
